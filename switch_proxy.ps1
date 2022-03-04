60b378ab4cb6267fc489b2e6fea617f94a91b359
ap-91 <65593692+ap-91@users.noreply.github.com>
2022-03-04 09:46:48 +0200

    Create switch_proxy.ps1

---
+++ switch_proxy.ps1
@@ -0,0 +1,11 @@
+﻿$proxy=Get-ItemPropertyValue -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings' -name 'ProxyServer'
+If ($proxy -eq '10.21.12.6:8080') {
+Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings' -name ProxyServer -Value "10.21.12.213:8080"
+}
+elseif ($proxy -eq '10.21.12.213:8080') {
+Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings' -name ProxyServer -Value "10.21.12.6:8080"
+}
+Else {
+Write-Error -Message "The proxy address is not valid"
+}
+Read-Host -Prompt 'press enter to finish'
